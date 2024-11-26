745ba5adff4d8537e8633ebdfdabb967e09070ec
Thong Nguyen <thongdatng201@gmail.com>
2024-11-26 08:13:29 +0700

    First commit

---
+++ EduTech/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduTech.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
