0e0ae6dbb4d3382be446f643e8d9ef749a6dcde7
suikax86 <suikax86@outlook.com>
2024-12-15 10:21:16 +0700

    Schedule using syncfusion

---
+++ EduTech/Models/ScheduleData.cs
@@ -0,0 +1,10 @@
+﻿namespace EduTech.Models
+{
+    public class ScheduleData
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; } = String.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
